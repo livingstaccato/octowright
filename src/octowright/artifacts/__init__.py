bb2a4ca91e92f5cea6659f9d1bc2c6d765c50d8f
Tim <code@tim.life>
2026-05-26 13:00:47 -0700

    feat(artifacts): add safe artifact path store

---
+++ src/octowright/artifacts/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (C) 2026 provide.io llc
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-Comment: Part of octowright.
+#
+
+"""Shared artifact helpers for Octowright automation outputs."""
